3e7234c4c0b61f38b6f250eb789c0b641a90613a
Nihal Sharma <gnihalsharma2021@gmail.com>
2026-06-12 11:11:37 +0530

    Building Student Profile Program

---
+++ student_profile.py
@@ -0,0 +1,7 @@
+name = input("Enter student name: ")
+age = input("Enter student age: ")
+city = input("Enter student city: ")
+
+print("Student name: ", name)
+print("Student age: ", age)
+print("Student city: ", city)
