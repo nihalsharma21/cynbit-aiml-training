8ff0a2fc6d4f4aed55fc448bfa86c4351ddc56bd
Nihal Sharma <gnihalsharma2021@gmail.com>
2026-06-15 12:05:31 +0530

    Grade Checker Program

---
+++ grade_checker.py
@@ -0,0 +1,9 @@
+marks = float(input("Enter student marks: "))
+if (marks >= 90):
+    print("Student grade is: A")
+elif (marks >= 75):
+    print("Student grade is: B")
+elif (marks >= 60):
+    print("Student grade is: C")
+else:
+    print("Fail")
