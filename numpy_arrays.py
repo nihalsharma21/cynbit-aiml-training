81e991baae3ca7d83e33efb58a55d59e9c052995
Nihal Sharma <gnihalsharma2021@gmail.com>
2026-06-22 14:58:33 +0530

    Numpy Arrays Basics Operations Created

---
+++ numpy_arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+arr1 = np.array([10,20,30,40,50])
+arr2 = np.array([2,4,6,8,10])
+print(f"The first array elements: {arr1}")
+print(f"The second array elements: {arr2}")
+# Applying Basic Operations
+print(f"Addition of array elements: {arr1 + arr2}")
+print(f"Subtraction of array elements: {arr1 - arr2}")
+print(f"Multiplication of array elements: {arr1 * arr2}")
+print(f"Division of array elements: {arr1 / arr2}")
